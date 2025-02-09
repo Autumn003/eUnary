4421a9927ab394659a1bf9ede5aa6cdd506a75ba
Hemant <davidbroddy1@gmail.com>
2025-02-09 11:49:06 +0530

    `Refactor Footer component: update layout, add links and social media`

--- apps/docs/components/Footer.tsx
+++ apps/docs/components/Footer.tsx
@@ -3,8 +3,8 @@ import Link from 'next/link';
 
 export default function Footer() {
     return (
-        <div className="p-8">
-            <div className="flex flex-col md:flex-row">
+        <div className="border-muted-background border-t px-8">
+            <div className="flex flex-col gap-10 py-14 md:flex-row md:gap-0">
                 <div className="w-full">
                     <Link
                         href="/"
@@ -40,24 +40,77 @@ export default function Footer() {
                         </div>
                     </div>
                 </div>
+                <div className="w-full"></div>
                 <div className="flex w-full">
-                    <div className="flex w-full flex-col items-end">
-                        <div className="text-secondary-foreground flex flex-col gap-8">
-                            <Link href="/components">Pricing</Link>
-                            <Link href="/components">Components</Link>
-                            <Link href="/components">Templates</Link>
-                            <Link href="/components">Posts</Link>
+                    <div className="flex w-full flex-col">
+                        <div className="text-secondary-foreground flex flex-col gap-8 text-sm">
+                            <Link
+                                href="/pricing"
+                                className="hover:text-primary-foreground transition-colors duration-200"
+                            >
+                                Pricing
+                            </Link>
+                            <Link
+                                href="/components"
+                                className="hover:text-primary-foreground transition-colors duration-200"
+                            >
+                                Components
+                            </Link>
+                            <Link
+                                href="/templates"
+                                className="hover:text-primary-foreground transition-colors duration-200"
+                            >
+                                Templates
+                            </Link>
+                            <Link
+                                href="/posts"
+                                className="hover:text-primary-foreground transition-colors duration-200"
+                            >
+                                Posts
+                            </Link>
                         </div>
                     </div>
-                    <div className="flex w-full flex-col items-end">
-                        section 2
+                    <div className="flex w-full flex-col">
+                        <div className="flex w-full flex-col">
+                            <div className="text-secondary-foreground flex flex-col gap-8 text-sm">
+                                <Link
+                                    href="https://x.com/Hemantsh03"
+                                    target="_blank"
+                                    className="hover:text-primary-foreground transition-colors duration-200"
+                                >
+                                    Twitter
+                                </Link>
+                                <Link
+                                    href="https://github.com/Autumn003/eUnary"
+                                    target="_blank"
+                                    className="hover:text-primary-foreground transition-colors duration-200"
+                                >
+                                    GitHub
+                                </Link>
+                            </div>
+                        </div>
                     </div>
-                    <div className="flex w-full flex-col items-end">
-                        section 3
+                    <div className="flex w-full flex-col">
+                        <div className="text-secondary-foreground flex flex-col gap-8 text-sm">
+                            <Link
+                                href="https://pro.eunary.com"
+                                target="_blank"
+                                className="hover:text-primary-foreground transition-colors duration-200"
+                            >
+                                UI Eunary Pro
+                            </Link>
+                            <Link
+                                href="https://eunary.com"
+                                target="_blank"
+                                className="hover:text-primary-foreground transition-colors duration-200"
+                            >
+                                Eunary
+                            </Link>
+                        </div>
                     </div>
                 </div>
             </div>
-            <footer className="text-secondary-foreground pt-10 text-center text-sm">
+            <footer className="text-secondary-foreground py-10 text-center text-sm">
                 <p>
                     © {new Date().getFullYear()} Eunary UI. All rights reserved
                 </p>
