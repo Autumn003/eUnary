74ce725fdf9fd0f3d8aec73524d614468cff5ce3
Kunal Sharma <davidbroddy1@gmail.com>
2025-02-24 16:27:27 +0530

    `refactor component-preview by adding tabs`

--- apps/docs/components/mdx/component-preview.tsx
+++ apps/docs/components/mdx/component-preview.tsx
@@ -1,9 +1,12 @@
-'use client';
-
-import React, { useState } from 'react';
 import { cn } from '@/lib/utils';
 import ComponentSource from './component-source';
 import 'remixicon/fonts/remixicon.css';
+import {
+    Tabs,
+    TabsContent,
+    TabsList,
+    TabsTrigger,
+} from '@/components';
 
 interface ComponentShowcaseProps {
     component: React.ReactNode;
@@ -16,46 +19,33 @@ const ComponentPreview: React.FC<ComponentShowcaseProps> = ({
     name,
     className,
 }) => {
-    const [showCode, setShowCode] = useState(false);
-    const [showComponent, setShowComponent] = useState(true);
-
-    const showCodeBtnHandler = () => {
-        setShowCode(true);
-        setShowComponent(false);
-    };
-    const showComponentBtnHandler = () => {
-        setShowComponent(true);
-        setShowCode(false);
-    };
 
     return (
         <div
             className={cn('no-scrollbar my-4 h-full rounded-lg p-4', className)}
         >
-            <div className="my-2 flex gap-2">
-                <button
-                    onClick={showComponentBtnHandler}
-                    className={`border-muted-background w-32 cursor-pointer rounded-lg border py-2 ${showComponent && 'bg-muted-background'} transition-colors duration-150`}
-                >
-                    <i className="ri-window-line"></i>{' '}
-                    <span className="text-sm font-semibold">Preview</span>
-                </button>
-                <button
-                    onClick={showCodeBtnHandler}
-                    className={`border-muted-background w-32 cursor-pointer rounded-lg border px-3 py-2 ${showCode && 'bg-muted-background'} transition-colors duration-150`}
-                >
-                    <i className="ri-terminal-box-line"></i>{' '}
-                    <span className="text-sm font-semibold">Code</span>
-                </button>
-            </div>
+            <Tabs defaultValue="preview">
+                <TabsList className='my-2 gap-4'>
+                    <TabsTrigger value="preview" className='data-[state=active]:bg-muted-background data-[state=active]:text-primary-foreground border-muted-background w-32 cursor-pointer rounded-lg border py-2  transition-colors duration-150 text-secondary-foreground'>
+                        <i className="ri-window-line"></i>{' '}
+                        <span className="text-sm font-semibold">Preview</span>
+                    </TabsTrigger>
+                    <TabsTrigger value="code" className='data-[state=active]:bg-muted-background data-[state=active]:text-primary-foreground border-muted-background text-secondary-foreground w-32 cursor-pointer rounded-lg border py-2  transition-colors duration-150'>
+                        <i className="ri-terminal-box-line"></i>{' '}
+                        <span className="text-sm font-semibold">Code</span>
+                    </TabsTrigger>
+                </TabsList>
 
-            {showComponent && (
-                <div className="border-muted-background flex min-h-96 items-center justify-center rounded-lg border p-4">
-                    <div className="rounded-lg border p-4">{component}</div>
-                </div>
-            )}
+                <TabsContent value="preview">
+                    <div className="border-muted-background flex min-h-96 items-center justify-center rounded-lg border p-4">
+                        <div className="rounded-lg border p-4">{component}</div>
+                    </div>
+                </TabsContent>
 
-            {showCode && <ComponentSource componentName={name} />}
+                <TabsContent value="code">
+                    <ComponentSource componentName={name} />
+                </TabsContent>
+            </Tabs>
         </div>
     );
 };
