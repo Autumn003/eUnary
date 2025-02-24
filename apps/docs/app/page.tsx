9fa03fc2726ba72a396e906f714a574e61c3eb48
Kunal Sharma <davidbroddy1@gmail.com>
2025-02-24 15:53:39 +0530

    `revert test commit`

--- apps/docs/app/page.tsx
+++ apps/docs/app/page.tsx
@@ -14,7 +14,6 @@ export default function page() {
             <AutoScrollingGallery />
             <ComponentExample />
             <Features />
-            {/* testing commit */}
         </div>
     );
 }
