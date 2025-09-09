636ee085c69c907382a5f937ec4fa9a6132a3021
Hemant <hemantsh003@gmail.com>
2025-09-09 06:24:22 +0000

    `Refactor ShowcasePage component: update CSS classes and fetchpriority`

--- apps/docs/app/components/page.tsx
+++ apps/docs/app/components/page.tsx
@@ -9,15 +9,15 @@ export default function ShowcasePage() {
         a.title.localeCompare(b.title)
     );
     return (
-        <div className="no-scrollbar grid grid-cols-2 gap-4 overflow-auto">
+        <div className="no-scrollbar grid grid-cols-2 gap-4 overflow-auto px-2">
             {sortedItems.map((item, index) => (
                 <Link
                     key={index}
                     href={`/components/${item.name}`}
-                    className="group col-span-2 m-3 h-fit rounded-2xl md:col-span-1"
+                    className="group col-span-2 m-3 mx-auto h-fit w-full rounded-2xl px-2 md:col-span-1"
                 >
                     <motion.div
-                        className="border-muted-background flex max-w-96 items-center justify-center overflow-hidden rounded-3xl border p-2 lg:max-w-full"
+                        className="flex max-w-80 flex-col items-start justify-center overflow-hidden rounded-3xl sm:max-w-96 lg:max-w-full"
                         whileHover={{
                             scale: 1.03,
                         }}
@@ -25,20 +25,21 @@ export default function ShowcasePage() {
                         <motion.img
                             src={item.thumbnail}
                             alt={`Illustrative image for ${item.title} components, ${item.description}`}
-                            className="h-44 w-full rounded-2xl object-cover sm:h-64"
+                            className="border-muted-background h-44 w-full rounded-3xl border object-cover p-2 sm:h-64"
+                            fetchPriority="high"
                             whileHover={{
                                 scale: 0.96,
                             }}
                         />
+                        <div className="px-2 sm:m-2 sm:px-4">
+                            <h2 className="dark:text-primary-foreground text-xl font-semibold text-neutral-800 transition-transform ease-in-out group-hover:scale-105">
+                                {item.title}
+                            </h2>
+                            <p className="text-secondary-foreground line-clamp-2 text-[15px] transition-transform ease-in-out group-hover:scale-95">
+                                {item.description}
+                            </p>
+                        </div>
                     </motion.div>
-                    <div className="px-2 sm:m-2 sm:px-4">
-                        <h2 className="dark:text-primary-foreground text-xl font-semibold text-neutral-800 transition-transform ease-in-out group-hover:scale-105">
-                            {item.title}
-                        </h2>
-                        <p className="text-secondary-foreground text-[15px] transition-transform ease-in-out group-hover:scale-95">
-                            {item.description}
-                        </p>
-                    </div>
                 </Link>
             ))}
         </div>
